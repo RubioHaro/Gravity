224a627833abf9ca0d20b3d01bca62667a1e004a
Rodrigo Rubio Haro <chavo0022009@gmail.com>
2020-04-20 04:30:37 -0500

    V0.1
    Se agregó la base del proyecto

---
+++ Gravity/src/main/java/com/rubio/haro/Gravity/GravityApplication.java
@@ -0,0 +1,13 @@
+package com.rubio.haro.Gravity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GravityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GravityApplication.class, args);
+	}
+
+}
